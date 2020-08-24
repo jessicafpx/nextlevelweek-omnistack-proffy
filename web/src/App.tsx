0f15a3bfe81854c9d1c9ef107deb7a6fec28c690
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2020-08-24 10:10:36 -0300

    Server, web and mobile

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing';
+import Routes from './routes';
+
+//JSX - JavaScript + XML
+
+
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
